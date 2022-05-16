2bedd9833b9e10037dfcc2cc9995db48c232e621
Andrew Belcher <belcher.w.andrew@gmail.com>
2022-05-16 14:09:52 +0800

    Commit map with color

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
-var projection = d3.geoMercator()
-  .scale(400)
-  .translate([200, 280])
-  .center([0, 5]);
+let height = 600;
+let width = 950;
 
-var geoGenerator = d3.geoPath()
-  .projection(projection);
+let svg = d3.select('svg')
+            .attr('height', height)
+            .attr('width', width);
 
-function update(geojson) {
-    var u = d3.select('#australia g.map')
-      .selectAll('path')
-      .data(geojson.features);
+let data = d3.geoPath(); 
+
+// Color Scale
+var colorScale = d3.scaleThreshold()
+                    .domain([1, 10, 20, 30, 40, 50])
+                    .range(d3.schemeBlues[7]);
+
+let counties_json = 
+'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+let education_json = 
+'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+
+d3.queue()
+  .defer(d3.json, counties_json)
+  .defer(d3.json, education_json)
+  .await(ready);
+
+function ready(error, data_count, data_edu) {
+
+  console.log(data_count);
+  console.log(data_edu);
   
-    u.enter()
-      .append('path')
-      .attr('d', geoGenerator)
-      // .on('mouseover', handleMouseover);
-}
+  svg.append("g")
+      .selectAll("path")
+      .data(topojson.feature(data_count, data_count.objects.counties).features)
+      .enter()
+      .append("path")
+      .attr("class", "county")
+      .attr("d", data)
+      .attr("fill", function(d) {
+        // console.log(d.id);
 
-// REQUEST DATA
-d3.json('africa.json', function(err, json) {
-    update(json);
-})
+        let county = data_edu.find(obj => {
+          return obj.fips == d.id;
+        })
+
+        // console.log(county);
+
+        return colorScale(county['bachelorsOrHigher']);
+      })
+      .attr('data-fips', function(d) {return d.id;}) //console.log(d);})
+      .attr('data-education', function(d) {
+        let county = data_edu.find(obj => {
+          return obj.fips == d.id;
+        })
+
+        return county['bachelorsOrHigher'];
+      })
+}
